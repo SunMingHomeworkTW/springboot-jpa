3b367e5a070cc9b89c5c4f7dcd702caf1466839a
SunMing <1229536801@qq.com>
2018-04-25 17:06:21 +0800

    在main下的entity中创建实体Company和Employee

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,49 @@
 package com.example.employee.entity;
 
 public class Company {
+    private int id;
+    private String companyName;
+    private int employeesNumber;
 
+    public Company() {
+    }
+
+    public Company(int id, String companyName, int employeesNumber) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", companyName='" + companyName + '\'' +
+                ", employeesNumber=" + employeesNumber +
+                '}';
+    }
 }
